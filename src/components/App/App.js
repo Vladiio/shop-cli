da301bccc43768e2ee1c3bc97ceece19736beb16
Vlad <vladpochta5@gmail.com>
2018-07-06 21:16:54 +0300

    Configure private route for HomePage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
+import PrivateRoute from '../../routes/privateRoute';
 import BookDetailPage from '../BookDetailPage/BookDetailPage';
 import SignUpPage from '../SignUpPage/SignUpPage';
 import LoginPage from '../LoginPage/LoginPage';
@@ -8,7 +9,7 @@ import LoginPage from '../LoginPage/LoginPage';
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={HomePage} />
+      <PrivateRoute path="/" exact Component={HomePage} />
       <Route path="/register" component={SignUpPage} />
       <Route path="/login" component={LoginPage} />
       <Route path="/books/:slug" component={BookDetailPage} />
---
+++ src/routes/privateRoute.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Route, Redirect } from 'react-router-dom';
+
+export default ({ Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={() => (localStorage.getItem('user') ? <Component /> : <Redirect to="/loging" />)}
+  />
+);
